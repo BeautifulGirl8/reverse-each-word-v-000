ab7d800d072027cd02564fcbc997d5f57f982656
Vicky  T <vttak9@gmail.com>
2017-10-15 20:59:37 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@ def reverse_each_word(phrase)
   split_sentence = phrase.split()
   new_words = []
   new_words << split_sentence.collect{|word| word.reverse}
-  new_words.join
+  new_words.join( )
 end
